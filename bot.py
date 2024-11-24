a8e300a15992d4f8c8bec35578bbc2acd2f2b173
Darkester <95033598+Clos-Rise@users.noreply.github.com>
2024-11-24 15:24:48 +0500

    sus v3

--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
-import telebot # библиотека telebot
-from config import token # импорт токена
+import telebot
 
-bot = telebot.TeleBot(token) 
+token = '7668163167:AAGZl6soDeYiSO9UenfwwLIa5gvrkq0GzHk'
+
+bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -9,18 +10,36 @@ def start(message):
 
 @bot.message_handler(commands=['ban'])
 def ban_user(message):
-    if message.reply_to_message: #проверка на то, что эта команда была вызвана в ответ на сообщение 
-        chat_id = message.chat.id # сохранение id чата
-         # сохранение id и статуса пользователя, отправившего сообщение
+    if message.reply_to_message:
+        chat_id = message.chat.id
         user_id = message.reply_to_message.from_user.id
-        user_status = bot.get_chat_member(chat_id, user_id).status 
-         # проверка пользователя
+        user_status = bot.get_chat_member(chat_id, user_id).status
         if user_status == 'administrator' or user_status == 'creator':
             bot.reply_to(message, "Невозможно забанить администратора.")
         else:
-            bot.ban_chat_member(chat_id, user_id) # пользователь с user_id будет забанен в чате с chat_id
+            bot.ban_chat_member(chat_id, user_id)
             bot.reply_to(message, f"Пользователь @{message.reply_to_message.from_user.username} был забанен.")
     else:
-        bot.reply_to(message, "Эта команда должна быть использована в ответ на сообщение пользователя, которого вы хотите забанить.")
+        bot.reply_to(message, "Эта команда должна быть использована в ответ на сообщение пользователя.")
+
+@bot.message_handler(commands=['happy'])
+def send_happy(message):
+    bot.reply_to(message, "😊")
+
+@bot.message_handler(commands=['sad'])
+def send_sad(message):
+    bot.reply_to(message, "😢")
+
+@bot.message_handler(func=lambda message: True)
+def check_message(message):
+    if "https://" in message.text:
+        chat_id = message.chat.id
+        user_id = message.from_user.id
+        user_status = bot.get_chat_member(chat_id, user_id).status
+        if user_status == 'administrator' or user_status == 'creator':
+            bot.reply_to(message, "Невозможно забанить администратора.")
+        else:
+            bot.ban_chat_member(chat_id, user_id)
+            bot.reply_to(message, f"Пользователь @{message.from_user.username} был забанен.")
 
 bot.infinity_polling(none_stop=True)
